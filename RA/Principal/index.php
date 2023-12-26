1301d91c545723bfb13a882d2361517a54cbbae1
fyael <152308171+fyael@users.noreply.github.com>
2023-12-25 19:13:25 -0600

    Add files via upload

---
+++ RA/Principal/index.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>
+    Realidad Aumentada
+  </title>
+  <link rel="stylesheet" href="Recursos/styles.css">
+
+  <link rel="stylesheet" href="Recursos/w3.css">
+</head>
+<body class ="back">
+
+
+<header class="header">
+        <div class="logo">
+            <img src="Recursos/juarez.png" alt="Primaria Benito Juarez">
+        </div>
+        <nav class="menu">
+           <ul class="nav-links">
+            <label id="titulo">Escuela Benito Juárez</label>
+            <li><a href="#"> Ayuda </a>
+            	<ul id="submenu">
+            		<li><a href="#">Contacto </a></li>
+            		<li><a href="#">Soporte </a></li>
+            	</ul>
+            </li>
+           </ul> 
+        </nav>
+
+    </header>
+    
+    <div class="Login">
+      <div id="btnacceder">
+        <button class="acceder" > Acceder </button>
+      </div>
+      
+      <div class="btnr" id="btnregistrar">
+       <button class="registrar" > Registrar </button>
+      </div>
+    </div>
+          
+
+          <div class="outer" id="text">
+            <div class="content">
+              <p class="heading">Realidad Aumentada</p>
+            <p class="texto">
+              El uso de las tecnologías está en su auge, y la realidad aumentada en el ámbito educativo cada vez es más frecuente. Por lo que “[NOMBRE DEL PROYECTO]” genera una relación alumno-docente donde la enseñanza es más divertida y entretenida.  
+            </p>
+                <button class="btn">INFO</button>
+
+            </div>
+
+    </div>
+
+
+
+</body>
+</html>
