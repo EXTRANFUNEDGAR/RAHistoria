d2b14281c3686ccd9bd7d5f7440ff922a962a7e8
Jesús Emmanuel Díaz Salas <emmanueldiaz882@gmail.com>
2024-03-13 23:32:55 -0600

    css login

--- login.php
+++ login.php
@@ -4,16 +4,39 @@
 <head>
    <meta charset="utf-8">
    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-   <link rel="stylesheet" href="css/bootstrap.css">
    <link rel="stylesheet" type="text/css" href="css/style.css">
    <link href="https://fonts.googleapis.com/css?family=Poppins:600&display=swap" rel="stylesheet">
    <link href="https://tresplazas.com/web/img/big_punto_de_venta.png" rel="shortcut icon">
    <title>Login</title>
+   <style>
+      /* Estilos CSS personalizados */
+
+      /* Contenedor principal */
+      body {
+         background-image: url('img/wave2.png'); /* Ruta de la imagen de fondo */
+         background-size: cover; /* Cubrir todo el área del cuerpo */
+         background-position: center; /* Centrar la imagen */
+         height: 100vh; /* Ajustar la altura del fondo */
+         margin: 0; /* Eliminar márgenes */
+         display: flex; /* Hacer el cuerpo un contenedor flexible */
+         justify-content: center; /* Centrar horizontalmente */
+         align-items: center; /* Centrar verticalmente */
+      }
+
+      .container {
+         background-color: rgba(255, 255, 255, 0.8); /* Color de fondo con opacidad */
+         padding: 50px;
+         border-radius: 10px;
+         box-shadow: 0 0 20px 0 rgba(0, 0, 0, 0.1); /* Sombra */
+      }
+
+      /* Resto de los estilos aquí */
+
+   </style>
 </head>
 
 <body>
-   <img class="wave" src="img/wave1.png">
+   <img class="wave" src="img/wave2.png">
    <div class="container">
       <div class="img">
       </div>
@@ -30,8 +53,7 @@
                   <i class="fas fa-user"></i>
                </div>
                <div class="div">
-                  <h5>Usuario</h5>
-                  <input id="usuario" type="text" class="input" name="usuario">
+                  <input id="usuario" type="text" class="input" name="usuario" placeholder="Usuario">
                </div>
             </div>
             <div class="input-div pass">
@@ -39,8 +61,7 @@
                   <i class="fas fa-lock"></i>
                </div>
                <div class="div">
-                  <h5>Contraseña</h5>
-                  <input type="password" id="input" class="input" name="password">
+                  <input type="password" id="password" class="input" name="password" placeholder="Contraseña">
                </div>
             </div>
             <div class="view">
@@ -55,12 +76,6 @@
          </form>
       </div>
    </div>
-   <script src="js/fontawesome.js"></script>
-   <script src="js/main.js"></script>
-   <script src="js/main2.js"></script>
-   <script src="js/jquery.min.js"></script>
-   <script src="js/bootstrap.js"></script>
-   <script src="js/bootstrap.bundle.js"></script>
 
 </body>
 
