8721998f4c494765b88218e4cfd09cb35b299c73
EXTRANFUNEDGAR <extranfunedgar@gmail.com>
2024-05-21 22:12:12 -0600

    segundo tema

---
+++ clases/RA/laIndependenciaMexicana/mem/mem.js
@@ -0,0 +1,147 @@
+const moves = document.getElementById("moves-count");
+const timeValue = document.getElementById("time");
+const startButton = document.getElementById("start");
+const stopButton = document.getElementById("stop");
+const gameContainer = document.querySelector(".game-container");
+const result = document.getElementById("result");
+const controls = document.querySelector(".controls-container");
+const firstCardPlaceholder = document.getElementById("first-card-placeholder");
+const secondCardPlaceholder = document.getElementById("second-card-placeholder");
+
+let cards;
+let interval;
+let firstCard = false;
+let secondCard = false;
+
+const items = [
+  { name: "img1", image: "img/1_1.jpg" },
+  { name: "img2", image: "img/2_2.jpg" },
+  { name: "img3", image: "img/3_3.jpg" },
+  { name: "img4", image: "img/4_4.jpg" },
+  { name: "img5", image: "img/5_5.jpg" },
+  { name: "img6", image: "img/6_6.jpg" },
+  { name: "img7", image: "img/7_7.jpg" },
+  { name: "img8", image: "img/8_8.jpg" },
+  { name: "img9", image: "img/9_9.jpg" },
+  { name: "img10", image: "img/10_10.jpg" },
+  { name: "img11", image: "img/11_11.jpg" },
+  { name: "img12", image: "img/12_12.jpg" },
+];
+
+let seconds = 0,
+  minutes = 0;
+let movesCount = 0,
+  winCount = 0;
+
+const timeGenerator = () => {
+  seconds += 1;
+  if (seconds >= 60) {
+    minutes += 1;
+    seconds = 0;
+  }
+  let secondsValue = seconds < 10 ? `0${seconds}` : seconds;
+  let minutesValue = minutes < 10 ? `0${minutes}` : minutes;
+  timeValue.innerHTML = `<span>Tiempo:</span>${minutesValue}:${secondsValue}`;
+};
+
+const movesCounter = () => {
+  movesCount += 1;
+  moves.innerHTML = `<span>Movimientos:</span>${movesCount}`;
+};
+
+const generateRandom = (size = 4) => {
+  let tempArray = [...items];
+  let cardValues = [];
+  size = (size * size) / 2;
+  for (let i = 0; i < size; i++) {
+    const randomIndex = Math.floor(Math.random() * tempArray.length);
+    cardValues.push(tempArray[randomIndex]);
+    tempArray.splice(randomIndex, 1);
+  }
+  return cardValues;
+};
+
+const matrixGenerator = (cardValues, size = 4) => {
+  gameContainer.innerHTML = "";
+  cardValues = [...cardValues, ...cardValues];
+  cardValues.sort(() => Math.random() - 0.5);
+  for (let i = 0; i < size * size; i++) {
+    gameContainer.innerHTML += `
+      <div class="card-container" data-card-value="${cardValues[i].name}">
+        <div class="card-before">?</div>
+        <div class="card-after">
+          <img src="${cardValues[i].image}" class="image" width="64px" height="64px"/>
+        </div>
+      </div>`;
+  }
+  gameContainer.style.gridTemplateColumns = `repeat(${size},auto)`;
+  cards = document.querySelectorAll(".card-container");
+  cards.forEach((card) => {
+    card.addEventListener("click", () => {
+      if (!card.classList.contains("matched")) {
+        card.classList.add("flipped");
+        if (!firstCard) {
+          firstCard = card;
+          firstCardValue = card.getAttribute("data-card-value");
+          firstCardPlaceholder.innerHTML = `<img src="${card.querySelector('.card-after img').src}" class="image" width="100%" height="100%"/>`;
+        } else {
+          movesCounter();
+          secondCard = card;
+          let secondCardValue = card.getAttribute("data-card-value");
+          secondCardPlaceholder.innerHTML = `<img src="${card.querySelector('.card-after img').src}" class="image" width="100%" height="100%"/>`;
+          if (firstCardValue == secondCardValue) {
+            firstCard.classList.add("matched");
+            secondCard.classList.add("matched");
+            firstCard = false;
+            secondCard = false;
+            firstCardPlaceholder.innerHTML = '';
+            secondCardPlaceholder.innerHTML = '';
+            winCount += 1;
+            if (winCount == Math.floor(cardValues.length / 2)) {
+              result.innerHTML = `<h2>Ganaste</h2>
+                <h4>Movimientos: ${movesCount}</h4>`;
+              stopGame();
+            }
+          } else {
+            let [tempFirst, tempSecond] = [firstCard, secondCard];
+            firstCard = false;
+            secondCard = false;
+            let delay = setTimeout(() => {
+              tempFirst.classList.remove("flipped");
+              tempSecond.classList.remove("flipped");
+              firstCardPlaceholder.innerHTML = '';
+              secondCardPlaceholder.innerHTML = '';
+            }, 900);
+          }
+        }
+      }
+    });
+  });
+};
+
+startButton.addEventListener("click", () => {
+  movesCount = 0;
+  seconds = 0;
+  minutes = 0;
+  controls.classList.add("hide");
+  stopButton.classList.remove("hide");
+  startButton.classList.add("hide");
+  interval = setInterval(timeGenerator, 1000);
+  moves.innerHTML = `<span>Movimientos:</span> ${movesCount}`;
+  initializer();
+});
+
+stopButton.addEventListener("click", (stopGame = () => {
+  controls.classList.remove("hide");
+  stopButton.classList.add("hide");
+  startButton.classList.remove("hide");
+  clearInterval(interval);
+}));
+
+const initializer = () => {
+  result.innerText = "";
+  winCount = 0;
+  let cardValues = generateRandom();
+  console.log(cardValues);
+  matrixGenerator(cardValues);
+};
--- clases/RA/losPrimerosHumanos/subir.php
+++ clases/RA/losPrimerosHumanos/subir.php
@@ -1,40 +1,38 @@
 <?php
-// Verificar si se ha recibido el valor "1" del formulario
+
 if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["respuesta"]) && $_POST["respuesta"] == 1) {
-    // Datos de conexión a la base de datos (modificar según tu configuración)
+    
     $servername = "localhost";
     $username = "root";
     $password = "";
     $dbname = "ra_db";
 
-    // Crear conexión
     $conn = new mysqli($servername, $username, $password, $dbname);
 
-    // Verificar la conexión
     if ($conn->connect_error) {
         die("La conexión ha fallado: " . $conn->connect_error);
     }
 
-    // Preparar la consulta para insertar el valor en la tabla 'avance'
     $stmt = $conn->prepare("INSERT INTO avance (id_usuario, id_tema, avance) VALUES (?, ?, ?)");
     $stmt->bind_param("iii", $id_usuario, $id_tema, $avance);
 
-    // Establecer los valores a insertar
-    $id_usuario = 1; // Cambiar por el ID del usuario correspondiente
-    $id_tema = 1; // Cambiar por el ID del tema correspondiente
+    $id_usuario = 1; 
+    $id_tema = 1; 
     $avance = 1;
 
-    // Ejecutar la consulta
     if ($stmt->execute()) {
-        echo "El avance se ha registrado correctamente en la base de datos.";
+        // Redirigir a la página después de la ejecución exitosa
+        header("Location: http://localhost/RAHistoria/clases/losPrimerosHumanos.html");
+        exit();
     } else {
         echo "Error al registrar el avance en la base de datos: " . $conn->error;
     }
 
-    // Cerrar la conexión y liberar recursos
     $stmt->close();
     $conn->close();
 } else {
     echo "No se ha recibido el valor 1 desde el formulario.";
 }
+
 ?>
+
