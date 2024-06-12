4b013c2ec7d6c14768989e21bb7e553e642f720e
Jesús Emmanuel Díaz Salas <emmanueldiaz882@gmail.com>
2024-06-11 21:30:00 -0600

    base de datos

--- base de datos/ra_db.sql
+++ base de datos/ra_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 07-05-2024 a las 02:59:49
+-- Tiempo de generación: 12-06-2024 a las 05:27:58
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
@@ -33,6 +33,14 @@ CREATE TABLE `avance` (
   `avance` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Volcado de datos para la tabla `avance`
+--
+
+INSERT INTO `avance` (`id_usuario`, `id_tema`, `avance`) VALUES
+(11, 3, 8),
+(10, 4, 8);
+
 -- --------------------------------------------------------
 
 --
@@ -43,6 +51,14 @@ CREATE TABLE `clases` (
   `id_clase` int(11) NOT NULL,
   `id_usuario` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `clases`
+--
+
+INSERT INTO `clases` (`id_clase`, `id_usuario`) VALUES
+(1, 10),
+(2, 11);
 
 -- --------------------------------------------------------
 
@@ -67,6 +83,14 @@ CREATE TABLE `temas` (
   `tema` varchar(100) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Volcado de datos para la tabla `temas`
+--
+
+INSERT INTO `temas` (`id_tema`, `id_clase`, `tema`) VALUES
+(3, 1, 'Los primeros Humanos'),
+(4, 2, 'Mexico Antiguo');
+
 -- --------------------------------------------------------
 
 --
@@ -75,10 +99,31 @@ CREATE TABLE `temas` (
 
 CREATE TABLE `usuarios` (
   `id_usuario` int(11) NOT NULL,
+  `nombre` varchar(50) DEFAULT NULL,
   `curp` varchar(18) DEFAULT NULL,
-  `contrasena` varchar(255) DEFAULT NULL,
+  `contrasena` varchar(500) DEFAULT NULL,
   `tipo` int(2) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `usuarios`
+--
+
+INSERT INTO `usuarios` (`id_usuario`, `nombre`, `curp`, `contrasena`, `tipo`) VALUES
+(1, '', 'pruebad', 'pruebad', 1),
+(2, '', 'prueba', 'prueba', 2),
+(3, '', 'prueba2', 'prueba2', 1),
+(4, '', 'prueba3', 'prueba3', 2),
+(5, '', 'prueba4', '1234', 1),
+(6, '', 'prueba5', '1', 1),
+(7, '', 'prueba6', '$2y$10$ASetS0JFxnAOp', 1),
+(8, '', 'c', '$2y$10$L1usaVkzwck8k', 2),
+(9, '', 'pruebac', '$2y$10$bBuXE9IAre07fLu/UvqnRuFNsGU.4i.vmffvj1UTPMYU3KVui.M.e', 1),
+(10, 'emmanuel', 'disj', '$2y$10$7llKinycM8/CYaCoY/uR3uchTmdNGHdel9jMnSMKgmv2lceyhfqX2', 1),
+(11, 'edgar', 'edgar', '$2y$10$o4Nq1FzxHfCPyY/mMqRVwO3sZ8Ti9DzcEEMtEeANeI7cBLC9/b3OC', 2),
+(12, 'enrique', 'enrique', '$2y$10$qv0WwYu10.5jUMiIs11zsOVGooo.S1/vucuqrgvO7izL4ZcLY74pK', 1),
+(13, 'p', 'pc', '$2y$10$q/KuLzwVtecXKuDj0HpUneCKJAPEvIs/faOUwqvLO1iU7HKL3cwqm', 2),
+(14, '1', '1', '$2y$10$.NCfnoPQ/plAMT1fObAzqe6/e5ncy8VqrZn6LCkW0rrssFM6ywW.C', 2);
 
 --
 -- Índices para tablas volcadas
@@ -95,7 +140,7 @@ ALTER TABLE `avance`
 -- Indices de la tabla `clases`
 --
 ALTER TABLE `clases`
-  ADD PRIMARY KEY (`id_clase`);
+  ADD UNIQUE KEY `id_clase` (`id_clase`);
 
 --
 -- Indices de la tabla `clase_usuario`
@@ -122,22 +167,16 @@ ALTER TABLE `usuarios`
 --
 
 --
--- AUTO_INCREMENT de la tabla `clases`
---
-ALTER TABLE `clases`
-  MODIFY `id_clase` int(11) NOT NULL AUTO_INCREMENT;
-
---
 -- AUTO_INCREMENT de la tabla `temas`
 --
 ALTER TABLE `temas`
-  MODIFY `id_tema` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_tema` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- Restricciones para tablas volcadas
--- logout.php
+++ logout.php
@@ -1,17 +1,12 @@
 <?php
-// Iniciar sesión si aún no está iniciada
 session_start();
 
-// Eliminar la variable de sesión específica
 unset($_SESSION['usuario']);
 
-// Destruir todas las demás variables de sesión
 $_SESSION = array();
 
-// Finalmente, destruir la sesión
 session_destroy();
 
-// Borrar la cookie de sesión si se creó
 if (isset($_COOKIE["session"])) {
     setcookie("session", "", time()-3600, "/");
     echo "Cookie de sesión eliminada correctamente.";
@@ -20,8 +15,6 @@ if (isset($_COOKIE["session"])) {
 }
 
 
-
-// Redirigir al usuario a la página de inicio o a cualquier otra página que prefieras
 header("Location: index.php");
 exit;
 
